700486bc7c846f004561f5ee174a3d515f9aa03b
Mauritz Vervaart <mjver@mweb.co.za>
2022-03-24 22:17:37 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
+/* eslint-disable */
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeaderVue :totalIncome="state.totalIncome" />
+    <FormVue :state="state" @add-income="AddIncome" />
+    <IncomeList :state="state" />
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, computed } from 'vue'
+import HeaderVue from './components/HeaderVue.vue'
+import FormVue from './components/FormVue.vue'
+import IncomeList from './components/IncomeList.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    setup() {
+        const state = reactive({
+            income: [],
+            totalIncome: computed(() => {
+                let temp = 0;
+                if (state.income.length > 0) {
+                  for (let i = 0; i < state.income.length; i++) {
+                      temp += state.income[i].value;
+                  }
+                  
+                } 
+                return temp;
+                
+            }),
+            sortedIncome: computed(() => {
+              let temp = [];
+
+              temp = state.income.sort((a, b) => {
+                return b.date - a.date;
+              })
+
+              return temp;
+            })
+        });
+
+        function AddIncome (data) {
+          let d = data.date.split("-");
+          let newD = new Date(d[0], d[1], d[2]);
+
+          state.income = [...state.income, {
+            id: Date.now(),
+            desc: data.desc,
+            value: parseInt(data.value),
+            date: newD.getTime()
+          }];
+          console.log(state.income)
+        }
+
+        return {
+            HeaderVue,
+            state,
+            FormVue,
+            AddIncome,
+            IncomeList
+        };
+    },
+    components: { HeaderVue, FormVue, IncomeList }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+body {
+  background: #EEE;
+
 }
 </style>
